f27bfcac6b98f8a176cd43aa26c4be23079934ad
Andrey Indenko <a.indenko@vanbrug.nl>
2014-07-23 16:08:23 +0300

    + get sqlite chats information

--- index.py
+++ index.py
@@ -1,3 +1,13 @@
 __author__ = 'andrey'
+import sqlite3
 
 print("Hello World!")
+
+#connect db
+conn = sqlite3.connect('main.db')
+#sql
+sql = "SELECT * FROM Chats LIMIT 10"
+
+#execute
+res = conn.execute(sql)
+print(res.fetchall())
