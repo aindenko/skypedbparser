0e76acf1a0b22c54f70e6c7e446c97c682b86835
Andrey Indenko <a.indenko@vanbrug.nl>
2014-07-23 17:20:28 +0300

    + get messages

--- index.py
+++ index.py
@@ -5,11 +5,22 @@ import settings
 
 print("Hello World!")
 
+from os.path import expanduser
+home = expanduser("~")
+
 #connect db
 conn = sqlite3.connect(settings.path + '/main.db')
 #sql
-sql = "SELECT * FROM Chats LIMIT 10"
+sql = "SELECT Chats.activity_timestamp FROM Chats WHERE name='"+settings.chat+"' ORDER by id desc "
 
 #execute
 res = conn.execute(sql)
 print(res.fetchall())
+
+
+sql = "SELECT * FROM Messages WHERE chatname='"+settings.chat+"' ORDER by id desc LIMIT 20"
+
+#execute
+res = conn.execute(sql)
+print(res.fetchall())
+print(home)
