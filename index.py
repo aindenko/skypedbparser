d35ae8e92cb47313fe4de2e68401e096d6463e56
Andrey Indenko <a.indenko@vanbrug.nl>
2014-07-24 14:10:51 +0300

    + use config and learning conditions

--- index.py
+++ index.py
@@ -1,11 +1,23 @@
 __author__ = 'andrey'
 import sqlite3
-import settings
 import configparser
 import logging
 
-logging.basicConfig(filename='my.log',filemode='w',level=logging.DEBUG)
+cfgfile = 'my.ini'
+#config load
+config = configparser.RawConfigParser()
+config.read(cfgfile)
 
+path = config.get('system', 'path')
+chat = config.get('system', 'chat')
+ts = config.get('system', 'ts')
+
+#store last ts
+#config.set('system', 'ts',lastts)
+
+
+logging.basicConfig(filename='my.log',filemode='a',level=logging.DEBUG)
+logging.debug('///////////////////////////////////////////////');
 logging.debug('Parser started');
 
 
@@ -15,31 +27,23 @@ logging.debug('Home dir is '+home);
 
 
 #connect db
-conn = sqlite3.connect(settings.path + '/main.db')
-#sql
+conn = sqlite3.connect(path + '/main.db')
+#sql check last activity ts
 sql = "SELECT Chats.activity_timestamp FROM Chats WHERE name=? ORDER by id desc "
 logging.debug(sql)
 #execute
-res = conn.execute(sql,[settings.chat])
-res.fetchall()
+res = conn.execute(sql,[chat])
+lastactts = res.fetchall()[0][0]
+
+if lastactts > ts :
+    sql = "SELECT * FROM Messages WHERE chatname= ? ORDER by id desc LIMIT 20"
+    logging.debug(sql)
+    #execute
+    res = conn.execute(sql,[chat])
+    logging.debug(res.fetchall())
 
 
-sql = "SELECT * FROM Messages WHERE chatname='"+settings.chat+"' ORDER by id desc LIMIT 20"
-logging.debug(sql)
-#execute
-res = conn.execute(sql)
-res.fetchall()
 
-
-#config load
-config = configparser.RawConfigParser()
-config.read('my.ini')
-
-path = config.get('system', 'path')
-chat = config.get('system', 'chat')
-
-#config.add_section('system')
-my = config.set('system', 'my',3)
 
 
 #config save
