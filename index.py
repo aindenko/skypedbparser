7defcd55102b5416df9505201ec651705be57f08
Andrey Indenko <a.indenko@vanbrug.nl>
2014-07-24 10:12:09 +0300

    + some debug added

--- index.py
+++ index.py
@@ -4,29 +4,34 @@ import settings
 import configparser
 import logging
 
+logging.basicConfig(filename='my.log',filemode='w',level=logging.DEBUG)
+
+logging.debug('Parser started');
 
 
 from os.path import expanduser
 home = expanduser("~")
+logging.debug('Home dir is '+home);
+
 
 #connect db
 conn = sqlite3.connect(settings.path + '/main.db')
 #sql
-sql = "SELECT Chats.activity_timestamp FROM Chats WHERE name='"+settings.chat+"' ORDER by id desc "
-
+sql = "SELECT Chats.activity_timestamp FROM Chats WHERE name=? ORDER by id desc "
+logging.debug(sql)
 #execute
-res = conn.execute(sql)
-print(res.fetchall())
+res = conn.execute(sql,[settings.chat])
+res.fetchall()
 
 
 sql = "SELECT * FROM Messages WHERE chatname='"+settings.chat+"' ORDER by id desc LIMIT 20"
-
+logging.debug(sql)
 #execute
 res = conn.execute(sql)
-print(res.fetchall())
-print(home)
+res.fetchall()
 
 
+#config load
 config = configparser.RawConfigParser()
 config.read('my.ini')
 
@@ -36,6 +41,7 @@ chat = config.get('system', 'chat')
 #config.add_section('system')
 my = config.set('system', 'my',3)
 
-print(path)
+
+#config save
 with open('my.ini', 'w') as configfile:
     config.write(configfile)
